bd73f98e11111f3f9fa651948475b5f71c1b1312
antrivera <anthony.rivs@gmail.com>
2016-08-26 16:20:50 -0700

    built redux store along with bench and root reducers

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+  window.Store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<div> it worked </div>, root);
 });
