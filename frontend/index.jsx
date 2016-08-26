09d08d41c54bf62ccb87e4c88791aea4154290cd
antrivera <anthony.rivs@gmail.com>
2016-08-26 16:41:22 -0700

    Created bench actions and middlware, and connected RootMiddleware to the Store

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import { requestBenches, receiveBenches } from './actions/bench_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   window.Store = configureStore();
+  window.requestBenches = requestBenches;
+  window.receiveBenches = receiveBenches;
   const root = document.getElementById('root');
   ReactDOM.render(<div> it worked </div>, root);
 });
