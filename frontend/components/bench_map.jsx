56412e3e17c126aae554696c202da9237eaf2d2b
antrivera <anthony.rivs@gmail.com>
2016-08-29 17:49:29 -0700

    integrated Google Map into BenchMap and refactored Search to use implicit return.

--- frontend/components/bench_map.jsx
+++ frontend/components/bench_map.jsx
@@ -5,6 +5,19 @@ class BenchMap extends React.Component {
     super();
   }
 
+  componentDidMount() {
+    const mapDOMNode = this.refs.map;
+
+    // display SF on map
+    const mapOptions = {
+      center: {lat: 37.7758, lng: -122.435},
+      zoom: 12
+    };
+
+    // wrap mapDOMNode in a Google Map
+    this.map = new google.maps.Map(mapDOMNode, mapOptions);
+  }
+
   render() {
     return (
       <div id={'map-container'} ref={'map'} ></div>
--- frontend/components/search.jsx
+++ frontend/components/search.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import BenchMap from './bench_map';
 import BenchIndex from './bench_index';
 
-const Search = ({benches, requestBenches}) => {
-  console.log(benches);
-
-  return (
+const Search = ({benches, requestBenches}) => (
   <div>
     <BenchMap />
     <BenchIndex benches={benches} requestBenches = {requestBenches} />
   </div>
-  );
-}
+);
 
 export default Search;
