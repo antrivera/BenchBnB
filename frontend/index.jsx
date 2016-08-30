7a687193c91aa37c744c36a315dbe25eaf14e81f
antrivera <anthony.rivs@gmail.com>
2016-08-30 14:15:53 -0700

    Included React Router and refactored the entry file accordingly.

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import configureStore from './store/store';
-import { requestBenches, receiveBenches } from './actions/bench_actions';
-import SearchContainer from './components/search_container';
-import { Provider } from 'react-redux';
-
-const Root = ({store}) => (
-  <Provider store={store}>
-    <SearchContainer />
-  </Provider>
-);
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = window.Store = configureStore();
-  window.requestBenches = requestBenches;
-  window.receiveBenches = receiveBenches;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
