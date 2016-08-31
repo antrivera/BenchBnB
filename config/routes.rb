035159dc318a3096412c4bcae410401c9837a86f
antrivera <anthony.rivs@gmail.com>
2016-08-31 10:28:28 -0700

    Implemented Auth backend: Added User model + api namespaced controller and api namespaced sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json}  do
     resources :benches, only: [:index, :create]
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
   end
 end
